52a84243bc0a2067d3e4a99ea7bd21e98f57b920
m252 <first.marcos@gmail.com>
2018-11-22 22:56:12 -0200

    jogada especial

---
+++ bin/chess.bat
@@ -0,0 +1,2 @@
+@Echo OFF
+java -jar xadrez.jar
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,13 +2,17 @@ package chess.pieces;
 
 import bordergame.Board;
 import bordergame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
+	
+	private ChessMatch chessMatch;
 
-	public King(Board board, Color color) {
+	public King(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
+		this.chessMatch = chessMatch;
 		
 	}
 	@Override
@@ -19,6 +23,10 @@ public class King extends ChessPiece {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p == null || p.getColor() != getColor();
 		
+	}
+	private boolean testRookCasteling(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p instanceof Rook && p.getColor()== getColor() &&p.getMovecount()==0;
 	}
 	@Override
 	public boolean[][] possibleMoves() {
@@ -63,6 +71,29 @@ public class King extends ChessPiece {
 				p.setValues(position.getRow() +1, position.getColumn()+1);
 				if (getBoard().positionExists(p) && canMove(p)) {
 					mat[p.getRow()][p.getColumn()]=true;
+				}
+				
+				//special move casteling
+				if(getMovecount()==0 && !chessMatch.getCheck()) {
+					//special move casteling king side rook
+					Position posT1 = new Position(position.getRow(),position.getColumn()+3);
+					if(testRookCasteling(posT1)) {
+						Position p1 = new Position(position.getRow(),position.getColumn()+1);
+						Position p2 = new Position(position.getRow(),position.getColumn()+2);
+						if(getBoard().piece(p1) == null && getBoard().piece(p2) == null) {
+							mat[position.getRow()][position.getColumn()+2] = true;
+						}
+					}
+				}
+				//special move queen side casteling
+				Position posT2 = new Position(position.getRow(),position.getColumn()-4);
+				if(testRookCasteling(posT2)) {
+					Position p1 = new Position(position.getRow(),position.getColumn()-1);
+					Position p2 = new Position(position.getRow(),position.getColumn()-2);
+					Position p3 = new Position(position.getRow(),position.getColumn()-3);
+					if(getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3)== null) {
+						mat[position.getRow()][position.getColumn()-2] = true;
+					}
 				}
 		return mat;
 	}
